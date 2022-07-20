6d88465125077308fe518348a440fc6f23e8e112
OlatunjI-Ruth <85808869+OlatunjI-Ruth@users.noreply.github.com>
2022-07-20 21:13:57 +0100

    Update appp.py

--- appp.py
+++ appp.py
@@ -30,13 +30,10 @@ def index_view():
 def predict():
     if request.method == 'POST':
         image = request.files['my_image']
-        #image_path =image.filename
-        #image.save(image_path)
-
+        image_path =image.filename
+        image.save(image_path)
         img = load_img(image, target_size=(150, 150))
         x = img_to_array(img)
-        """ x = x[:, :, 0]
-        x = x.reshape(1, 150, 150, 3)"""
         x = np.expand_dims(x, axis=0)
         image_tensor = np.vstack([x])
         classes = model.predict(image_tensor)
