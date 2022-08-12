ebca0459a6433fd1d12bf5d11c1569dddeebe468
OlatunjI-Ruth <85808869+OlatunjI-Ruth@users.noreply.github.com>
2022-08-12 18:45:28 +0100

    Delete loader.py

--- loader.py
+++
@@ -1,7 +0,0 @@
-from numpy import loadtxt
-from tensorflow.keras.models import load_model
-
-
-def init():
-    model = load_model(r"C:\Users\QueenHenny\PycharmProjects\Cats_vs_Dogs\model\model.h5")
-    return model
